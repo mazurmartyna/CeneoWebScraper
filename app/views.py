cf4afdf297d25bd6c13199bd957386ca8aef2ca5
Martyna Mazur <smileymarti3@gmail.com>
2025-05-21 11:56:34 +0200

    2025-05-21 MM

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 import os
 import json
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from flask import render_template, request, redirect, url_for
@@ -20,6 +21,19 @@ def display_form():
 def extract():
     product_id = request.form.get('product_id')
     next_page = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+    response = requests.get(next_page, headers=headers)
+    if response.status_code == 200:
+        page_dom = BeautifulSoup(response.text, "html.parser")
+        product_name = utils.extract_feature(page_dom, "h1")
+        opinions_count = utils.extract_feature(page_dom, "a.product-review__link > span")
+        if not opinions_count:
+            error="Dla produktu o podanym id nie ma jeszcze żadnych opinii."
+            return render_template("extract.html", error=error)
+    else:
+        error="Nie znaleziono produktu o danym id"
+        return render_template("extract.html", error = error)
+        
+
     all_opinions = []
     while next_page:
         print(next_page)
@@ -45,7 +59,7 @@ def extract():
         os.mkdir("./app/data/opinions")
     with open(f"./app/data/opinions/{product_id}.json","w",encoding="UTF-8") as jf:
         json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
-    return redirect(url_for('product', product_id=product_id))
+    return redirect(url_for('product', product_id=product_id, product_name = product_name))
 
 @app.route("/products")
 def products():
@@ -57,4 +71,6 @@ def author():
 
 @app.route("/product/<product_id>")
 def product(product_id):
-    return render_template("product.html", product_id=product_id)
+    product_name = request.args.get('product_name')
+    opinions = pd.read_json(f"./app/data/opinions/{product_id}.json")
+    return render_template("product.html", product_id=product_id, product_name = product_name, opinions = opinions.to_html(table_id="opinions", classes=["display"]))
