8200ba4fdccea2c5430becf7f031090be0ff3e18
Martyna Mazur <smileymarti3@gmail.com>
2025-06-11 09:59:11 +0200

    2025-06-10 MM

--- app/views.py
+++ app/views.py
@@ -13,7 +13,6 @@ import matplotlib
 matplotlib.use('Agg')
 
 @app.route("/")
-@app.route("/<name>")
 def index(name="World"):
     return render_template("index.html")
 
@@ -42,7 +41,7 @@ def extract():
     while next_page:
         print(next_page)
         response = requests.get(next_page, headers=headers)
-        if response.status_code ==200:
+        if response.status_code == 200:
             page_dom = BeautifulSoup(response.text, "html.parser")
             opinions = page_dom.select("div.js_product-review:not(.user-post--highlight)")
             for opinion in opinions:
@@ -57,6 +56,13 @@ def extract():
                 next_page = None
         else: print(response.status_code)
     
+    if not os.path.exists("./app/data"):
+        os.mkdir("./app/data")
+    if not os.path.exists("./app/data/opinions"):
+        os.mkdir("./app/data/opinions")
+    with open(f"./app/data/opinions/{product_id}.json","w",encoding="UTF-8") as jf:
+        json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+
     opinions = pd.DataFrame.from_dict(all_opinions)
     opinions.stars = opinions.stars.apply(lambda s: s.split("/")[0].replace(",", ".")).astype(float)
     opinions.useful = opinions.useful.astype(int)
@@ -72,16 +78,15 @@ def extract():
         "average_stars": float(opinions.stars.mean()),
         "pros": opinions.pros.explode().dropna().value_counts().to_dict(),
         "cons": opinions.cons.explode().dropna().value_counts().to_dict(),
-        "recommendations": opinions.recommendation.value_counts(dropna=False).reindex(['Nie polecam','Polecam', None], fill_value=0).to_dict()
+        "recommendations": opinions.recommendation.value_counts(dropna=False).reindex(['Nie polecam','Polecam', None], fill_value=0).to_dict(),
     }
 
     if not os.path.exists("./app/data"):
         os.mkdir("./app/data")
     if not os.path.exists("./app/data/products"):
         os.mkdir("./app/data/products")
-    with open(f"./app/data/opinions/{product_id}.json","w",encoding="UTF-8") as jf:
+    with open(f"./app/data/products/{product_id}.json","w",encoding="UTF-8") as jf:
         json.dump(stats, jf, indent=4, ensure_ascii=False)
-    
     return redirect(url_for('product', product_id=product_id, product_name = product_name))
 
 @app.route("/products")
@@ -92,7 +97,6 @@ def products():
         with open(f"./app/data/products/{filename}","r", encoding="UTF-8") as jf:
             product = json.load(jf)
             products_list.append(product)
-
     return render_template("products.html", products=products_list)
 
 @app.route("/author")
@@ -102,7 +106,7 @@ def author():
 @app.route("/product/<product_id>")
 def product(product_id):
     product_name = request.args.get('product_name')
-    with open(f"./app/data/opinions/{product_id}.json","r",encoding="UTS-8") as jf:
+    with open(f"./app/data/opinions/{product_id}.json","r",encoding="UTF-8") as jf:
         opinions = json.load(jf)
     return render_template("product.html", product_id=product_id, product_name = product_name, opinions = opinions)
 
@@ -113,12 +117,10 @@ def charts(product_id):
         os.mkdir("./app/static/images")
     if not os.path.exists("./app/static/images/charts"):
         os.mkdir("./app/static/images/charts")
-    with open(f"./app/data/opinions/{product_id}.json","w",encoding="UTF-8") as jf:
-        json.dump(stats, jf, indent=4, ensure_ascii=False)
-    with open(f"./app/data/opinions/{product_id}.json0", "r") as jf:
+    with open(f"./app/data/products/{product_id}.json", "r", encoding="UTF-8") as jf:
         stats = json.load(jf)
-        recommendations = pd.series(stats["recommendations"])
-        recommendations.plot.pie(
+    recommendations = pd.Series(stats["recommendations"])
+    recommendations.plot.pie(
         label="",
         title=f"Rozkład rekomendacji w opiniach o {product_id}", 
         labels=['Nie polecam','Polecam','Nie mam zdania'],
